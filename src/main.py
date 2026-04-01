5b15e2e45155e82ebedca59bfb9fd737f83dfe95
Filip <fs300379@student.polsl.pl>
2026-04-01 10:01:14 +0200

    chosen data and detection of pedestrians

--- src/main.py
+++ src/main.py
@@ -1,21 +1,23 @@
 import cv2
 import os
+import natsort
 
-path = r"G:\Python\Synthia\Stereo_Left\Omni_F"
+path = r"/home/filip/projekt_pism/train/chosen_data/19-10-2018_13-33-29/RGB"
+image_files = [f for f in os.listdir(path) if f.endswith(".png")]
+image_files = natsort.natsorted(image_files)
 
-for img_name in os.listdir(path):
+for img_name in image_files:
 
     img_path = os.path.join(path, img_name)
-
     img = cv2.imread(img_path)
 
     if img is None:
         print("Nie wczytano:", img_path)
         continue
 
-    cv2.imshow("Preview", img)
+    cv2.imshow("Image View", img)
 
-    if cv2.waitKey(100) == 27:
+    if cv2.waitKey(17) == 27:
         break
 
 cv2.destroyAllWindows()
--- src/second.py
+++ src/second.py
@@ -1,45 +1,40 @@
 import cv2
 import os
+import natsort
 from ultralytics import YOLO
 
 # wczytanie modelu YOLO
 model = YOLO("yolov8n.pt")
 
-path = r"G:\Python\Synthia\Stereo_Left\Omni_F"
+path = r"/home/filip/projekt_pism/train/chosen_data/19-10-2018_13-33-29/RGB"
+image_files = [f for f in os.listdir(path) if f.endswith(".png")]
+image_files = natsort.natsorted(image_files)
 
-for img_name in os.listdir(path):
+for img_name in image_files:
 
-    if not img_name.lower().endswith(".png"):
-        continue
 
     img_path = os.path.join(path, img_name)
 
     img = cv2.imread(img_path)
 
     if img is None:
+        print("Nie wczytano:", img_path)
         continue
 
-    # detekcja
-    results = model(img)
+    # detekcja tylko ludzi i z pewnoscia 60%
+    results = model(img, classes = [0],conf = 0.6, verbose=False)
 
     # obraz z bounding boxami
     frame = results[0].plot()
 
     # liczenie pieszych
-    boxes = results[0].boxes
-    people_count = 0
-
-    for box in boxes:
-        cls = int(box.cls[0])
-
-        if cls == 0:  # klasa 0 = person
-            people_count += 1
+    people_count = len(results[0].boxes)
 
     print(f"{img_name} -> pedestrians: {people_count}")
 
     cv2.imshow("YOLO detection", frame)
 
-    if cv2.waitKey(50) == 27:
+    if cv2.waitKey(33) == 27:
         break
 
 cv2.destroyAllWindows()
