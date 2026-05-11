48bc8004791a4c5a7bd36389a4eb01d9697b8715
Mateusz Jania <mateusz.jania01@gmail.com>
2026-05-11 21:42:32 +0200

    testowanie rónych modeli yolo8

--- src/pedestrian_detection.py
+++ src/pedestrian_detection.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 people_counts = []
 
 # wczytanie modelu YOLO
-model = YOLO("yolov8m.pt")
+model = YOLO("yolov8n.pt")
 
 path = r"G:\Python\Synthia\Stereo_Left\Omni_F"
 
